c521ce58e6e7ba555e851a7c733a993d57c740e2
ltybc157 <den.ste21013@yandex.ru>
2019-04-10 12:48:04 +0600

    коммит  lesson 111.1

---
+++ js/modal.js
@@ -0,0 +1,11 @@
+"use strict";
+ let btn1 = document.querySelector('.header_btn'),
+ btn2 = document.querySelector('.text-uppercase'),
+ btn3 = document.querySelector('.text-left'),
+ btn4 = document.querySelector('.popup_engineer_btn');
+ //close = document.querySelector();
+ console.log(btn1);
+ console.log(btn2);
+ console.log(btn3);
+ console.log(btn4);
+
