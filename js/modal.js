09299897be9e0158924cdb1b46014e57aaa95d2e
ltybc157 <den.ste21013@yandex.ru>
2019-04-10 14:21:10 +0600

    коммит  lesson 111.2

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,33 @@
-"use strict";
- let btn1 = document.querySelector('.header_btn'),
- btn2 = document.querySelector('.text-uppercase'),
- btn3 = document.querySelector('.text-left'),
- btn4 = document.querySelector('.popup_engineer_btn');
- //close = document.querySelector();
- console.log(btn1);
- console.log(btn2);
- console.log(btn3);
- console.log(btn4);
+window.addEventListener('DOMContentLoaded', function () {
+ let btn = document.querySelector('.popup_engineer_btn'),
+ modal =document.querySelector('.popup_engineer'),
+ close = document.querySelector('.popup_close');
+ header = document.querySelector('.header');
+ 
+ btn.addEventListener('click', function () {
+    modal.style.display = 'flex';
+    this.classList.add('more-splash');
+   document.body.style.overflow = 'hidden';
+ });
 
+ close.addEventListener('click', function () {
+    modal.style.display = 'none';
+    header.style.display = 'none';
+    btn.classList.remove('more-splash');
+   document.body.style.overflow = '';
+ });
+
+
+ 
+ console.log(close);
+
+
+
+
+
+
+
+
+
+
+});
